1b959873208c39abb03b787fe6bed58a73f13088
Megan Kaiser <meganrayanne@yahoo.com>
2018-02-10 14:11:59 -0700

    Search working, upvote works but doesn't go to storage

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ var $saveBttn = $('.save-idea-bttn');
 var $ideaSection = $('.ideas');
 var $title = $('.title-input');
 var $body = $('.body-input');
+var $search = $('.search-input');
 
 var qualityArray = ['swill', 'plausible', 'genius'];
 
 // event listeners
 $saveBttn.on('click', makeIdea);
 $ideaSection.on('click', '.delete-bttn', deleteIdea);
-// $ideaSection.on('click', '.upvote-bttn', ideaUpvote);
+$ideaSection.on('click', '.upvote-bttn', ideaUpVote);
+$ideaSection.on('click', '.downvote-bttn', ideaDownVote);
+$search.on('keyup', searchIdeas);
 
 
 // on page load
@@ -21,13 +24,14 @@ function makeIdea(e) {
   newIdea.createIdea();
   addIdeaToStorage(newIdea)
   clearFields();
+  $title.focus();
 }
 // Makeidea prepend time title body quailty 
-function Idea(title, body, newId) {
+function Idea(title, body, newId, qualityArray) {
   this.newId = newId || $.now();
   this.title = title;
   this.body = body;
-  this.quality = 'swill'; 
+  this.quality = qualityArray || 0; 
 } 
 
 Idea.prototype.createIdea = function () {
@@ -37,7 +41,7 @@ Idea.prototype.createIdea = function () {
         <p class="user-idea-body" contenteditable="true">${this.body}</p>
         <button class="upvote-bttn"></button>
         <button class="downvote-bttn"></button>
-        <p class="quailty">quality: <span class="quailty-placeholder">${this.quality}</span></p>
+        <p class="quality">quality: <span class="quality-placeholder">${qualityArray[this.quality]}</span></p>
       </article>`);
 }
 
@@ -57,9 +61,20 @@ function getIdeaFromStorage() {
   for(i = 0; i < localStorage.length; i++) {
     var getObject = localStorage.getItem(localStorage.key(i));
     var parseObject = JSON.parse(getObject);
-    var persistIdea = new Idea(parseObject.$title, parseObject.$body, parseObject.newId, parseObject.quality);
-    persistIdea.createIdea();
+    var newIdea = new Idea(parseObject.title, parseObject.body, parseObject.newId, parseObject.qualityArray);
+    createNewIdea(parseObject);
   }
+}
+
+function createNewIdea(object) {
+  $ideaSection.prepend(` <article class="user-idea" id="${this.newId}">
+        <h2 class="user-idea-title" contenteditable="true">${this.$title}</h2>
+        <button class="delete-bttn"></button>
+        <p class="user-idea-body" contenteditable="true">${this.$body}</p>
+        <button class="upvote-bttn"></button>
+        <button class="downvote-bttn"></button>
+        <p class="quality">quality: <span class="quality-placeholder">${qualityArray[this.quailty]}</span></p>
+      </article>`);
 }
 
 // delete bttn and stroage
@@ -71,16 +86,42 @@ function deleteIdea() {
 }
 
 // upvote bttn
-// function ideaUpvote() {
-//   if () {
-
-//   }  
-// }
+function ideaUpVote() {
+  var parentCard = this.closest('article').id;
+  if ($('.quality-placeholder').text() === 'swill') {
+    $('.quality-placeholder').text(qualityArray[1]);
+  }  else if ($('.quality-placeholder').text() === 'plausible') {
+    $('.quality-placeholder').text(qualityArray[2]);
+  }
+}
 
 // downvote bttn
+function ideaDownVote() {
+  var parentCard = this.closest('article');
+  var pulledCard = JSON.parse(localStorage.getItem(parentCard.id));
+  // console.log(pulledCard.quality)
+  if ($('.quality-placeholder').text() === 'genius') {
+
+    // localStorage.setItem(parentCard.qualityArray)
+  }  else if ($('.quality-placeholder').text() === 'plausible') {
+    $('.quality-placeholder').text(qualityArray[0]);
+    console.log(pulledCard.quality)
+  }
+}
 
 // edit idea to storage
 
 // filter idea
+function searchIdeas() {
+  var $ideaTitle = $('.user-idea-title');
+  var $ideaBody = $('.user-idea-body');
+  var filtered = $(this).val();
+  for (var i = 0; i < ($ideaTitle.length); i++) {
+    $($ideaTitle[i]).parent('article').hide();
+   if ($($ideaTitle[i]).text().includes(filtered) || $($ideaBody[i]).text().includes(filtered)) {
+    $($ideaTitle[i]).parent('article').show();
+   } 
+  }
+}
 
 //  sorting
